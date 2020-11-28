c628fb07d90586e9ab58f6456ea4308a9425db1c
Luis Rudge <luis@luisrudge.net>
2020-11-28 20:55:20 -0300

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>API Dias Úteis</div>;
+  return (
+    <div>
+      <h1>API de dias úteis</h1>
+      <h3>Calcular o próximo dia útil:</h3>
+      <code>https://diasuteis.dev/api/proximo?data=2020-11-28</code>
+      <pre>{`{"proximoDiaUtil":"2020-11-30"}`}</pre>
+    </div>
+  );
 }
