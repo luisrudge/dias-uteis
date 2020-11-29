cdac26ea26ee137cbea8815d96042df8ba8c78b7
Luis Rudge <luis@luisrudge.net>
2020-11-29 02:56:50 -0300

    remover /proximo

--- pages/api/proximo.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { parseDate, proximo, formatDate } from './../../utils';
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    proximoDiaUtil: formatDate(proximo(parseDate(<string>req.query.data))),
-  });
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,6 @@ export default function Home() {
   return (
     <div>
       <h1>API de dias úteis</h1>
-      <h3>Calcular o próximo dia útil:</h3>
-      <code>https://diasuteis.dev/api/proximo?data=2020-11-28</code>
-      <pre>{`{"proximoDiaUtil":"2020-11-30"}`}</pre>
       <h3>Calcular o prazo em dias úteis:</h3>
       <code>https://diasuteis.dev/api/prazo?dias=10</code>
       <pre>{`{"diaUtil":"2020-12-11"}`}</pre>
